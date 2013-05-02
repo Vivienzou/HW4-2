0beb9fee5da573410214cb10e23de9ff5e876211
Vivienzou <zoul@uw.edu>
2013-05-01 17:51:10 -0700

    Update README.py

--- README.py
+++ README.py
@@ -14,4 +14,4 @@ for i in range(10**7+1):
         return b
         
 
-stats.TimeSeries([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]).plot_histogram()
+stats.TimeSeries([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]).plot_histogram(b)
